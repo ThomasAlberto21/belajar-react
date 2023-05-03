63c37f07cdedde9530b1327dc0eb381324a9b818
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-03 09:57:59 +0700

    .

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import BlogPost from '../BlogPost/BlogPost';
+import BlogPost from '../pages/BlogPost/BlogPost';
 // import FunctionalComponent from './components/FunctionalComponent/FunctionalComponent';
 // import StatefullComponent from './components/StatefullComponent/StatefullComponent';
-import Video from '../../components/props/video';
-import Product from '../Product/product';
-import LifeCycleComp from '../LifeCycleComp/LifeCycleComp';
+// import Video from '../components/props/video';
+import Product from '../pages/Product/product';
+import LifeCycleComp from '../pages/LifeCycleComp/LifeCycleComp';
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import './home.css';
 
@@ -20,13 +20,11 @@ class Home extends React.Component {
           <div className='navigation'>
             <Link to='/'>Blog Post</Link>
             <Link to='/product'>product</Link>
-            <Link to='/video'>video</Link>
             <Link to='/lifecycle'>lifecycle</Link>
           </div>
           <Routes>
             <Route path='/' exact element={<BlogPost />} />
             <Route path='/product' element={<Product />} />
-            <Route path='/video' element={<Video />} />
             <Route path='/lifecycle' element={<LifeCycleComp />} />
           </Routes>
         </>
