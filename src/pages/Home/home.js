f61f476c78c3bfb6189fc7d02f45534eaf487d5b
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-03 09:41:46 +0700

    .

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -6,7 +6,7 @@ import Video from '../../components/props/video';
 import Product from '../Product/product';
 import LifeCycleComp from '../LifeCycleComp/LifeCycleComp';
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
-import './home.css'
+import './home.css';
 
 class Home extends React.Component {
   state = {
@@ -16,33 +16,6 @@ class Home extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        {/* <h3>Video</h3>
-        <Video
-          rating='8.50'
-          title='Orang Utan 1'
-          desc='sdsdsadhsdansjdsjbdssd'
-        />
-        <Video rating='7.50' title='Orang Utan 2' desc='dasascsadkpaslda;sd' />
-        <Video
-          rating='5.30'
-          title='Orang Utan 3'
-          desc='sdacvscnjhdjsbdsdsdhs'
-        />
-        <Video
-          rating='7.90'
-          title='Orang Utan 4'
-          desc='sdasnccskasdsaksdmsds'
-        />
-        <Video />
-        <hr />
-
-        <Product />
-        <hr />
-        {this.state.showComponents ? <LifeCycleComp /> : null}
-        <hr />
-
-        <BlogPost /> */}
-
         <>
           <div className='navigation'>
             <Link to='/'>Blog Post</Link>
